831db01b315d0e8802bef2eede8ef7910aedda32
Natalya <natasundukova2005@gmail.com>
2024-03-17 16:37:42 +0400

    add todo app

---
+++ src/routes/project/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+    <title>Портфолио</title>
+</svelte:head>
+
+<div class='container'>
+    <h1>Мои проекты</h1>
+    <div class='apps'>
+        {#each routes as page}
+        <div class='app'>
+            <h2>{page.title}</h2>
+            <p>{page.body}</p>
+            <p class='readmore'>
+                <a class='link'href='/projects/apps/page.id'>
+                    Читать далее
+                </a>
+            </p>
+        </div>            
+        {/each}
+    </div>
+</div>
+
+<script>
+    import { routes } from './routes.js';
+</script>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+    .apps {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+    .app {
+        border: 1px solid #acabab;
+        padding: 10px;
+        box-shadow: 0 0 10px #c7c6c6;
+    }
+    .link {
+        color: rgb(12, 124, 168);
+    }
+</style>
